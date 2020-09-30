614c4315d788914b3d239541687d79fe71cbd39f
Jamesspitz <James.spitz1@gmail.com>
2020-09-30 11:44:30 -0500

    Updates

---
+++ precache-manifest.518fdd06baebd80eb3144196aadffc1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0324c63724f4cf1344be9d1a1f9f188",
+    "url": "/JS-App/index.html"
+  },
+  {
+    "revision": "41cf01aacbb12ef2ca1a",
+    "url": "/JS-App/static/css/main.39c29b59.chunk.css"
+  },
+  {
+    "revision": "49234c72d00790aeb47c",
+    "url": "/JS-App/static/js/2.7445faff.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/JS-App/static/js/2.7445faff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cf01aacbb12ef2ca1a",
+    "url": "/JS-App/static/js/main.e65c388d.chunk.js"
+  },
+  {
+    "revision": "5b0de7b75d2e4b72b5df",
+    "url": "/JS-App/static/js/runtime-main.a1589045.js"
+  },
+  {
+    "revision": "f4127463d35f3ed9682faf9dd38b3792",
+    "url": "/JS-App/static/media/DBLayoutWUD.f4127463.png"
+  },
+  {
+    "revision": "ecbad849e31a644b5c4afba3cfd937f3",
+    "url": "/JS-App/static/media/ProfilePicture1.ecbad849.png"
+  },
+  {
+    "revision": "043bbfa84f6ae56022e61698c303acfe",
+    "url": "/JS-App/static/media/WUDMobileSample.043bbfa8.png"
+  },
+  {
+    "revision": "656887a4bc27aeacdfcbe5f96faaffb7",
+    "url": "/JS-App/static/media/WUDSiteSample.656887a4.png"
+  },
+  {
+    "revision": "3e9c6993ee54098f1239258e4c688800",
+    "url": "/JS-App/static/media/mountain.3e9c6993.jpg"
+  },
+  {
+    "revision": "63d66d3d312f76da5dddb88ec20a3626",
+    "url": "/JS-App/static/media/mountain.63d66d3d.jpeg"
+  }
+]);
